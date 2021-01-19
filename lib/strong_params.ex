ffda002cb4df292b386738e0ba567c35a84ea9d8
DanielWSx64 <danielws.x64@gmail.com>
2021-01-19 10:22:59 -0300

    Initialize project

---
+++ lib/strong_params.ex
@@ -0,0 +1,18 @@
+defmodule StrongParams do
+  @moduledoc """
+  Documentation for `StrongParams`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StrongParams.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/strong_params_test.exs
@@ -0,0 +1,8 @@
+defmodule StrongParamsTest do
+  use ExUnit.Case
+  doctest StrongParams
+
+  test "greets the world" do
+    assert StrongParams.hello() == :world
+  end
+end
